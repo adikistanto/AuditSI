f563e28e5ed35fead20d21fd371911955046493b
adikistanto <adikistanto1@gmail.com>
2015-12-15 11:53:34 +0700

    hardware inventory

--- www/js/app.js
+++ www/js/app.js
@@ -23,3 +23,29 @@
 
 
 // ...additional event handlers here...
+angular.module('starter', ['ionic', 'ngCordova', 'starter.services', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+    if (window.cordova) {
+        //quick check for presence of ngCordovaFile plugin
+        console.log('starting cordovaFile check');
+        console.log($cordovaFile);
+        //check for the docs directory suing File plugin
+        $cordovaFile.checkDir(cordova.file.documentsDirectory, "")
+        .then(function(success) {
+            console.log(success);
+        }, function(error) {
+            console.log(error);
+        });
+    } else { console.log( ' no cordova plugins available ' ); }
+  });
+})
